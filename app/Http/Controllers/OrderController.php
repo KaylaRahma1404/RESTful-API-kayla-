3fcc33a84960ece0ec18d8b3ab6064c0287fbd1f
unknown <kaylarahma32@gmail.com>
2025-02-11 07:35:42 +0700

    uh

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Order::all(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'product_id' => 'required|exists:products,id',
+            'quantity' => 'required|integer',
+            'total_price' => 'required|numeric',
+        ]);
+
+        $order = Order::create($validated);
+        return response()->json($order, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $order = Order::findOrFail($id);
+        return response()->json($order, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $order = Order::findOrFail($id);
+        $validated = $request->validate([
+            'product_id' => 'sometimes|exists:products,id',
+            'quantity' => 'sometimes|integer',
+            'total_price' => 'sometimes|numeric',
+        ]);
+
+        $order->update($validated);
+        return response()->json($order, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $order = Order::findOrFail($id);
+        $order->delete();
+        return response()->json(null, 204);
+    }
+}
